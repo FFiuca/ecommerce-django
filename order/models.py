5392385eb4ea8f5715dcb4df41b07db13ae4db3e
unknown <fardana.fiuca31@gmail.com>
2023-10-11 20:57:19 +0700

    cp

--- order/models.py
+++ order/models.py
@@ -24,3 +24,25 @@ class Cart(SafeDeleteModel):
 
     class Meta:
         ordering = ['created_at']
+
+class Checkout(SafeDeleteModel):
+    _safedelete_policy = SOFT_DELETE
+
+    checkout_code = models.CharField(blank=False, max_length=100, unique=True)
+    customer = models.ForeignKey(uModel.Customer, related_name='checkout_customer', on_delete=models.CASCADE)
+    user_store = models.ForeignKey(sModel.UserStore, related_name='checkout_user_store', on_delete=models.CASCADE)
+    status = models.ForeignKey(mModel.Status, related_name='checkout_status', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(blank=True, auto_now=True)
+
+    class Meta:
+        ordering = ['created_at']
+        db_tablespace = 'order_checkout'
+        indexes = [
+            models.Index(fields=['customer', 'user_store_id'])
+        ]
+
+class CheckoutDetail(SafeDeleteModel):
+    pass
+
+
