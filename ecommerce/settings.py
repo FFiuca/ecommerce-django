ecfc9d843bbcea3e2ac32f67e668e6da6922c848
FFiuca <fardana.fiuca31@gmail.com>
2023-10-29 22:42:30 +0700

    silky

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -18,6 +18,7 @@ from django.conf import settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# print(BASE_DIR)
 
 env = environ.Env(
     # default setting if there not exist
@@ -247,6 +248,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -4,12 +4,15 @@ from django.urls import path, include
 urlpatterns = [
     # system
     path('admin/', admin.site.urls),
-    path("__debug__/", include("debug_toolbar.urls")),
-    path('api-auth/', include('rest_framework.urls')),
     path('account/', include('django.contrib.auth.urls')),
 
     # app
     path('user', include('user.urls')),
     path('store', include('store.urls')),
     path('order', include('order.urls')),
+
+    # third
+    path("__debug__/", include("debug_toolbar.urls")),
+    path('api-auth/', include('rest_framework.urls')),
+    path('silk/', include('silk.urls', namespace='silk')),
 ]
